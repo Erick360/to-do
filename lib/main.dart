abec94afb4cf55f188574ac1e011ed74220d8101
Eri Romero Perez <eri360roperez@gmail.com>
2025-03-25 20:28:11 -0600

    adding task functions

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/screens/tasks_screens.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+    home: TasksScreen(),
+    );
+  }
+}
---
+++ lib/screens/tasks_screens.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/screens/add_task_screen.dart';
+import 'package:to_do/widgets/tasks_list.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) => const AddTaskScreen(),
+          );
+        },
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'TO DO',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              //height: 300.0,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20.0),
+                      topLeft: Radius.circular(20.0))),
+              child: const TaskList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
